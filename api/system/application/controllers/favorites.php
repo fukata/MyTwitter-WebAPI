b642c4fd7cf477796e1768b426d0d54fb78b63ca
smeghead <smeghead@www9329u.sakura.ne.jp>
2011-03-04 23:38:38 +0900

    add favorites post function.

---
+++ api/system/application/controllers/favorites.php
@@ -0,0 +1,24 @@
+<?php
+class Favorites extends MY_Controller {
+	/**
+	 * @var twitter
+	 */
+	public $twitter;
+	
+	public function __construct() {
+		parent::__construct();
+		$this->load->library('twitter');
+	}
+	
+	public function create_post() {
+		$id = $this->input->post('id');
+		if ($id==false && mb_strlen($id)==0) {
+			$this->response(array());
+			return;
+		}
+		$params = array();
+		$res = $this->twitter->favorites_create($id, $params);
+		$this->response($res);
+	}
+}
+?>
